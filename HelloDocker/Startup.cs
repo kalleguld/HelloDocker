e514763330579019cea8de1d587a43e2a58400c3
Kasper Guldmann Nielsen <kgn@tricon.dk>
2019-09-17 14:53:10 +0200

    spa is now served by "ng serve" during development

--- HelloDocker/Startup.cs
+++ HelloDocker/Startup.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -32,34 +33,16 @@ namespace HelloDocker
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
 
-
-            var cachePeriod = TimeSpan.FromHours(24);
-            var fileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(
-                    Path.Combine(Directory.GetCurrentDirectory(), "spa/dist/spa"));
-            var staticFileOptions = new StaticFileOptions
-            {
-                FileProvider = fileProvider,
-                OnPrepareResponse = ctx =>
-                {
-                    ctx.Context.Response.Headers.Add("Cache-Control", $"public, max-age={cachePeriod.TotalSeconds}");
-                },
-            };
-
-
-            app.UseDefaultFiles(new DefaultFilesOptions
-            {
-                FileProvider = fileProvider
-            });
-            app.UseStaticFiles(staticFileOptions);
             app.UseMvc();
             app.UseSpaStaticFiles();
             app.UseSpa(conf =>
             {
+                conf.Options.SourcePath = "spa/dist/spa";
+                if (env.IsDevelopment())
+                {
+                    conf.UseAngularCliServer(npmScript: "start");
+                }
             });
         }
     }
